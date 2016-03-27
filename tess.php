e706ba99f5d0e728ec9d3eb5655218d126d720a2
aditmayapada <adityamayapada@Adityas-MacBook-Pro.local>
2016-03-27 10:47:25 +0700

    1

--- tess.php
+++ tess.php
@@ -1,15 +1,4 @@
 <?php 
-
-
-		// $content = "https://twitter.com/share.php?xxx";
-  //        if(preg_match('#https?://(www\.)?twitter\.com/(\w*)#', $content, $matches))    
-  //        var_dump($matches); 
-require_once 'lib/simple_html_dom.php'; 
-include 'function.php';
-
-	if (aktif('http://www.acehtenggarakab.go.id')) {
-		echo 'okebong';
-	}
- 
+echo "<h2>Simple Twitter API Test</h2>";
 
 ?>

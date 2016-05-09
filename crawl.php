d1e0753687b03ae99c64b5fa563689fe4c3e3b99
aditmayapada <adityamayapada@Adityas-MacBook-Pro.local>
2016-05-09 11:44:50 +0700

    1

--- crawl.php
+++ crawl.php
@@ -1,6 +1,6 @@
 
 <?php 
-	require_once 'lib/simple_html_dom.php';
+   require_once 'lib/simple_html_dom.php';
    include 'connection.php';
    include 'function.php';
    require_once('lib/TwitterAPIExchange.php');
@@ -37,6 +37,7 @@
                   $urltwit = findtwitter($isi);
                   if (empty($urltwit)){
                      $twit = 0;
+                     $twitter_lu = 0;
                   } else {
                      $twit = 1;
                      $twitter_lu =  get_twitter_lu($urltwit);
@@ -57,9 +58,10 @@
                   }
                   
 
-                  #get twitter is update -under construction
+                  #get twitter is update
+                  
                   if ($mycsvfile[$i-1][0] == $i) {
-                     if ($mycsvfile[$i-1][13] == $twitter_lu ) {
+                     if ($mycsvfile[$i-1][16] == $twitter_lu ) {
                         $twitter_update = 0;
                      } else{
                         $twitter_update = 1;
@@ -94,7 +96,7 @@
                $perda = perda($isiarr);
 
                #under construction
-               $buku_tamu = 0;
+               $buku_tamu = 1;
                $pagerank = 0;
                $facebook_update =0;
 
@@ -117,11 +119,12 @@
                'urltwit'      => $twit, #13
                'urlfb'        => $fb, #14
                'urlyt'        => $yt, #15
-               'twitter_update'    => $twitter_update, #16
-               'youtube_update'    => $youtube_update, #17
-               'facebook_update'   => $facebook_update, #18
-               'web_update'   => $web_update, #19
-               'isi'          => $isi #20
+               'twitter_lu'   => $twitter_lu, #16
+               'twitter_update'    => $twitter_update, #17
+               'youtube_update'    => $youtube_update, #18
+               'facebook_update'   => $facebook_update, #19
+               'web_update'   => $web_update, #20
+               'isi'          => $isi #21
             );
             
             array_push($semualist, $list);
@@ -144,32 +147,37 @@
       if ($fh !== false) {
          while (($data = fgetcsv($fh , 1000 , "," )) !== false){
             #Total Score Logic
+            $selayang_pandang = ($data[6] * 0.1946 + $data[7] * 0.2016 + $data[5] * 0.1933 + $data[4] * 0.1983 + $data[9] * 0.2122) ;
+            $kelengkapan = ($data[3] * 0.2059 + $data[10] * 0.2036 + $data[11] * 0.1971 + $data[8] * 0.1964 + $selayang_pandang * 0.1988) ;
+            $score_website = ($data[2] * 0.2630 + $data[20] * 0.2630 + $data[12] * 0.2394 + $kelengkapan * 0.2341) ;
+            $score_sosmed =  ((($data[13] * 0.5 + $data[17] * 0.5) * 0.3667)+ (($data[14] * 0.5 + $data[18] * 0.5) * 0.4778) + (($data[15] * 0.5 + $data[19] * 0.5) * 0.1156));
 
-            $kelengkapan = $data[3] * 0.2059 +
-            $totalScore = ($data[2] * 0.5 + ($data[3] * 0.33 + $data[4] * 0.33 + $data[5] * 0.33) * 0.2 + $data[6] * 0.3)*100  ;
+            $total_score = ($score_website * 0.5425 + $score_sosmed * 0.4575);
+            
+            $sql = "INSERT INTO `result`(`date`, `id_pemda`, `website`, `kelengkapan_website`, `selayang_pandang`, `sejarah`, `motto_daerah`, `lambang`, `lokasi`, `visi_misi`, `pemerintahan_daerah`,`geografi`, `peraturan_daerah`, `buku_tamu`, `SEO_pagerank`, `frekuensi_update`, `frekuensi_aktif`, `sosial_media`, `facebook`, `twitter`, `youtube`, `fu_facebook`, `fu_twitter`, `fu_youtube`, `nilai_total`) 
+            VALUES (CURRENT_DATE(),".$data[0].",".$score_website.",".$kelengkapan.",".$selayang_pandang.",".$data[6].",".$data[5].",".$data[4].",".$data[9].",".$data[7].",".$data[8].",".$data[3].",".$data[10].",".$data[11].",".$data[12].",".$data[20].",".$data[2].",".$score_sosmed.",".$data[14].",".$data[13].",".$data[15].",".$data[19].",".$data[17].",".$data[18].",".$total_score.");";
 
-            $sql = "INSERT INTO tes (`date`, `id_pemda`, `isaktif` , `twitter` , `facebook` ,`youtube` ,`webupdate`,`totalscore`) 
-            VALUES ( CURRENT_DATE(),".$data[0].",'". $data[2] ."','". $data[3] ."','".$data[4]."','".$data[5]."',".$data[6].",".$totalScore.");";
             $result = $conn->query($sql);
 
-            echo $sql;
 
+
+            
          }
 
       }
       #END INSERT TO MYSQL
 
-      #WEBPAGENEW.CSV == WEBPAGEOLD.CSV
-      $input   = fopen('cachedPages/webpagenew.csv', 'r');  //open for reading
-      $output  = fopen('cachedPages/webpageold.csv', 'w');  //open for writing
+      // #WEBPAGENEW.CSV == WEBPAGEOLD.CSV
+      // $input   = fopen('cachedPages/webpagenew.csv', 'r');  //open for reading
+      // $output  = fopen('cachedPages/webpageold.csv', 'w');  //open for writing
 
-      while (($data = fgetcsv($input , 1000 , ",")) !== false ) {
-         fputcsv($output, $data);
-      }
+      // while (($data = fgetcsv($input , 1000 , ",")) !== false ) {
+      //    fputcsv($output, $data);
+      // }
 
-      fclose($input);
-      fclose($output);
-      #END OF WEBPAGENEW.CSV == WEBPAGEOLD.CSV
+      // fclose($input);
+      // fclose($output);
+      // #END OF WEBPAGENEW.CSV == WEBPAGEOLD.CSV
 
 
 
